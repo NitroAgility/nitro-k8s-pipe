35324b1e9c50686698c8621ffc8bb2ef4155cea8
Nicola Gallo <nicolagmt@hotmail.com>
2020-06-28 18:32:47 +0200

    Amended script

--- get_nitro_k8s_pipe.sh
+++ get_nitro_k8s_pipe.sh
@@ -24,4 +24,4 @@
 
 [ -f nitro_k8s_docker_pipe.sh ] || curl -fsSL -o nitro_k8s_docker_pipe.sh https://raw.githubusercontent.com/NitroAgility/nitro-k8s-pipe/master/nitro_k8s_docker_pipe.sh
 [ -f nitro_k8s_pipe.sh ] || curl -fsSL -o nitro_k8s_pipe.sh https://raw.githubusercontent.com/NitroAgility/nitro-k8s-pipe/master/nitro_k8s_pipe.sh
-[ -f Dockerfile ] || curl -fsSL -o Dockerfile https://raw.githubusercontent.com/NitroAgility/nitro-k8s-pipe/master/Dockerfile
+[ -f Dockerfile ] || curl -fsSL -o NitroDockerfile https://raw.githubusercontent.com/NitroAgility/nitro-k8s-pipe/master/Dockerfile
